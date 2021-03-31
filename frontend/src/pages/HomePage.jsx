ca1fa47f4e9215b8dc6c250e148446b33485c24c
Micklerick617 <mlennon@smu.edu>
2021-03-30 20:09:32 -0500

    Modifying includes

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-//import React, { Component } from 'react';
+import React, { Component } from 'react';
 import {Link, Redirect} from 'react-router-dom';
 
 export class HomePage extends React.Component {
