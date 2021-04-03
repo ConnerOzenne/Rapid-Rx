2c25a6e65b10da491d3d17737844d35bfece616c
Edward-jiang495 <ejiang@smu.edu>
2021-04-03 11:16:43 -0500

    user story 9.1,9.2,9.3 finished

--- backend/routes/pharmacy.js
+++ backend/routes/pharmacy.js
@@ -33,4 +33,35 @@ module.exports = function pharmacy(app, logger) {
         });
     });
 
+
+    //get pharmacy location by manager 
+    //user story 9.1
+    app.get('/pharmacyLocation', (req, res) => {
+        // obtain a connection from our pool of connections
+        pool.getConnection(function (err, connection){
+            if(err){
+                // if there is an issue obtaining a connection, release the connection instance and log the error
+                logger.error('Problem obtaining MySQL connection',err)
+                res.status(400).send('Problem obtaining MySQL connection'); 
+            } else {
+                // if there is no issue obtaining a connection, execute query and release connection
+                connection.query('select name, address, city, state, zipcode, country from pharmacies join addresses on pharmacies.addressID = addresses.addressID', function (err, rows, fields) {
+                    // if there is an error with the query, release the connection instance and log the error
+                    connection.release()
+                    if (err) {
+                        logger.error("Error while fetching values: \n", err);
+                        res.status(400).json({
+                            "data": [],
+                            "error": "Error obtaining values"
+                        })
+                    } else {
+                        res.status(200).json({
+                            "data": rows
+                        });
+                    }
+                });
+            }
+        });
+    });
+
 }
