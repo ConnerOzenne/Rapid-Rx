f7f1a635409348035d7578a83de75f69576fd5e3
Austin Liu <37671748+ostenloo@users.noreply.github.com>
2021-04-03 11:54:09 -0500

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -103,5 +103,34 @@ module.exports = function user(app, logger) {
             }
         });
     });
+    
+    app.get('/getEmployees/:pharmacyID', (req, res ) => {
+        console.log(req.params.pharmacyID)
+        pool.getConnection(function (err, connection){
+            if(err){
+                // if there is an issue obtaining a connection, release the connection instance and log the error
+                logger.error('Problem obtaining MySQL connection',err)
+                res.status(400).send('Problem obtaining MySQL connection'); 
+            } else {
+                var pharmacyID = req.params.pharmacyID
+                // if there is no issue obtaining a connection, execute query and release connection
+                connection.query('SELECT * FROM `rapidrx`.`users` AS u WHERE u.pharmacyID = ? && u.authorityLevel = 1', [pharmacyID], function (err, rows, fields) {
+                    // if there is an error with the query, release the connection instance and log the error
+                    connection.release()
+                    if (err) {
+                        logger.error("Error while fetching values: \n", err);
+                        res.status(400).json({
+                            "data": [],
+                            "error": "Error obtaining values"
+                        })
+                    } else {
+                        res.status(200).json({
+                            "data": rows
+                        });
+                    }
+                });
+            }
+        });
+    }); 
 
 }
