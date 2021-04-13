81ed91f58d335a74cd44a30834689342d35501c7
ashrajesh <ashwinr@smu.edu>
2021-04-13 18:11:09 -0500

    Update order.js

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -90,4 +90,31 @@ module.exports = function order(app, logger) {
             }
         });
     });
+
+    // GET /orders/:userID
+    app.get('/order/:userID', (req, res) => {
+        // obtain a connection from our pool of connections
+        pool.getConnection(function (err, connection) {
+            if (err) {
+                // if there is an issue obtaining a connection, release the connection instance and log the error
+                logger.error('Problem obtaining MySQL connection', err)
+                res.status(400).send('Problem obtaining MySQL connection');
+            } else {
+                // if there is no issue obtaining a connection, execute query and release connection
+                connection.query('SELECT * FROM `rapidrx`.`orders` AS o WHERE o.userID = ?', [userID], function (err, rows, fields) {
+                    // if there is an error with the query, release the connection instance and log the error
+                    connection.release()
+                    if (err) {
+                        logger.error("Error while fetching values: \n", err);
+                        res.status(400).json({
+                            "data": [],
+                            "error": "Error obtaining values"
+                        });
+                    } else {
+                        res.status(200).json(rows);
+                    }
+                });
+            }
+        });
+    });
 }
