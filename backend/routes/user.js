62b5274943ec7d95ba7c0d97b9511f3108f182d1
ashrajesh <ashwinr@smu.edu>
2021-04-22 08:52:01 -0500

    remove duplicate user/:userID/orders

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -234,36 +234,6 @@ module.exports = function user(app, logger) {
     });
 
 
-    app.get('/user/:userID/orders', (req, res) => {
-        // obtain a connection from our pool of connections
-        pool.getConnection(function (err, connection){
-            if(err){
-                // if there is an issue obtaining a connection, release the connection instance and log the error
-                logger.error('Problem obtaining MySQL connection',err)
-                res.status(400).send('Problem obtaining MySQL connection'); 
-            } else {
-                var userID = req.params.userID
-                // if there is no issue obtaining a connection, execute query and release connection
-                connection.query('SELECT * FROM `rapidrx`.`orders` AS o WHERE o.userID = ?', [userID], function (err, rows, fields) {
-                    // if there is an error with the query, release the connection instance and log the error
-                    connection.release()
-                    if (err) {
-                        logger.error("Error while fetching userID orders: \n", err);
-                        res.status(400).json({
-                            "data": [],
-                            "error": "Error obtaining values"
-                        });
-                    } else {
-                        res.status(200).json({
-                            "data": rows
-                        });
-                    }
-                });
-            }
-        });
-    });
-
-
 //update addresses
 app.put('/user/:userID/addresses', (req, res) => {
     // obtain a connection from our pool of connections
